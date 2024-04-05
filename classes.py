27bd9f1d704f13fd6bc5c469977864a4e568da8b
Vera Appazova <appazovavs@gmail.com>
2024-04-06 00:15:31 +0300

    task2 ready

--- classes.py
+++ classes.py
@@ -7,6 +7,16 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lec(self, lector, course, grade):
+        if isinstance(lector, Lecturer) and course in self.courses_in_progress and course in lector.courses_attached:
+            if course in lector.grades:
+                lector.grades[course] += [grade]
+            else:
+                lector.grades[course] = [grade]
+        else:
+            return "Ошибка"
+
+
 
 class Mentor:
     def __init__(self, name, surname):
@@ -14,6 +24,18 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+        #self.courses_attached = []
+
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        #self.courses_attached = []
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -24,22 +46,23 @@ class Mentor:
             return 'Ошибка'
 
 
-class Lecturer(Mentor):
-    pass
-
-
-class Reviewer(Mentor):
-    pass
-
-
 best_student = Student('Ruoy', 'Eman', 'your_gender')
 best_student.courses_in_progress += ['Python']
 
 cool_mentor = Mentor('Some', 'Buddy')
 cool_mentor.courses_attached += ['Python']
 
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
-cool_mentor.rate_hw(best_student, 'Python', 10)
 
-print(best_student.grades)
+best_lector = Lecturer('Appazova', 'Katya')
+best_lector.courses_attached += ['java script']
+best_lector.courses_attached += ['Python']
+
+print(best_lector.courses_attached)
+
+best_reviewer = Reviewer('Hardi', 'Neytonovich')
+best_reviewer.courses_attached += ['Python']
+
+best_student.rate_lec(best_lector, 'Python', 10)
+best_student.rate_lec(best_lector, 'java script', 3)
+
+print(best_lector.grades)
